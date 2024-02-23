0f5379d85df4e32bc80a8408584ef21d07dde7b9
socratesgitgdddasd <gwon880328@gmail.com>
2024-02-23 15:23:07 +0900

    App.js 주석추가

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Body from './components/Body'
+//멋찐희재 (푸시할떄 지우세요)
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Body/>
     </div>
   );
