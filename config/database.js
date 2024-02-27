f75cd1dbf398e431488cc140dc650419250de457
socratesgitgdddasd <gwon880328@gmail.com>
2024-02-27 16:34:06 +0900

    로그인 회원가입 추가

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
-            host:'project-db-s.smhrd.com',
+            host:'project-db-campus.smhrd.com',
             user:'campus_23IS_IoT2_P2_2',
             password:'smhrd2',
-            port:'3307',
+            port:3307,
             database:'campus_23IS_IoT2_P2_2'
         });
 
--- react-project/src/components/Login.jsx
+++ react-project/src/components/Login.jsx
@@ -1,14 +1,63 @@
-/* "로그인 페이지"입니다.
-- 데이터베이스에서 id,pw 불러와 사용자가 적은 값과 비교
-    => 맞으면 실시간관리페이지(MainLive)로 이동
-    => 틀리면 alert창 띄운 후, 로그인 페이지로 다시 이동
-- sign up 버튼 누르면 회원가입 페이지(Signup)로 이동
-*/
-import React from 'react'
+import React, { useRef } from 'react'
+import {Form, Button} from 'react-bootstrap';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
 
 const Login = () => {
+
+  // useRef 생성 - id, pw
+  const idRef = useRef();
+  const pwRef = useRef();
+
+  const navigate = useNavigate();
+
+  /** 로그인 기능을 해주는 handleLogin 함수 구현 */
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    axios
+        .post('/user/login', {id: idRef.current.value, pw: pwRef.current.value})
+        .then((res)=>{
+          // 로그인 성공 : success | 로그인 실패 : fail
+          if(res.data.result === 'success'){
+            alert('로그인 성공!');
+
+            sessionStorage.setItem('user',JSON.stringify(res.data.user));
+
+            // window.location.href = '/main';
+            navigate('/mainlive');
+          }else{
+            alert('아이디 혹은 비밀번호를 확인해주세요.');
+            navigate('/login');
+          }
+        });
+  }
+  
   return (
-    <div>Login</div>
+    <div>
+    <h1>로그인</h1>
+      <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="formBasicID">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="id" placeholder="Enter id" ref={idRef}/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" ref={pwRef}/>
+        </Form.Group>
+
+
+        <div className='d-grid gap mb-3'>
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+  {/* 회원가입 링크 추가 */}
+  <p>계정이 없으신가요? <Link to="/signin">회원가입</Link></p>
+    </div>
   )
 }
 
